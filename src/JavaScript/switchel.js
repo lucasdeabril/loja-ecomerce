11c76d4afe76ce5a796ae3d9bba0a283a514debe
lucasdeabril <lucaspassosdesouza08@gmail.com>
2023-05-12 01:53:55 -0400

    adição do historico e perfil

--- src/JavaScript/switchel.js
+++ src/JavaScript/switchel.js
@@ -3,57 +3,18 @@ export function switchel (e) {
     let carrinho = document.querySelector('.carrinho')
     let favorito = document.querySelector('.favoritos')
     let loja = document.querySelector('.container')
-    if (e == 2){
-        if(loja.classList.contains('hide')){
-
-            if (favorito.classList.contains('hide')){
-                loja.classList.toggle('hide') 
-                carrinho.classList.toggle('hide')
-            }else{
-                loja.classList.toggle('hide')
-                favorito.classList.toggle('hide')
-            }
-            
-            
-        }else{
-            console.log(loja)
-        }
-    }
+    let perfil = document.querySelector('.criarperfil')
+    let slid = document.querySelector('.slid')
     if (e == 1){
-        if(favorito.classList.contains('hide')){
-
-            if (loja.classList.contains('hide')){
-                favorito.classList.toggle('hide') 
-                carrinho.classList.toggle('hide')
-            }else{
-                loja.classList.toggle('hide')
-                favorito.classList.toggle('hide')
-            }
-            
-            
-        }else{
-            console.log(loja)
-        }
+       slid.style.marginLeft='0px'
+    }
+    if (e == 2){
+       slid.style.marginLeft='-100%'
     }
     if (e == 3){
-        if(carrinho.classList.contains('hide')){
-
-            if (favorito.classList.contains('hide')){
-                loja.classList.toggle('hide') 
-                carrinho.classList.toggle('hide')
-            }else{
-                carrinho.classList.toggle('hide')
-                favorito.classList.toggle('hide')
-            }
-            
-            
-        }else{
-            console.log(loja)
-        }
+       slid.style.marginLeft='-200%'
     }
-        
-        
-
-    
-    
+    if (e == 4){
+       slid.style.marginLeft='-300%'
+    }
 }
